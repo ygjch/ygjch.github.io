5ca63e3404d1eb623f842ffe01d4e02e6631612b
ygjch <wwworzzzzzzzzzz@gmail.com>
2017-09-18 18:31:31 +0800

    Site updated: 2017-09-18 18:31:30

--- js/photo.js
+++ js/photo.js
@@ -4,7 +4,7 @@ define([], function () {
             offset: 20,
             init: function () {
                 var that = this;
-                $.getJSON("/source/photo/output.json", function (data) {
+                $.getJSON("/photo/output.json", function (data) {
                     that.render(that.page, data);
 
                     that.scroll(data);
@@ -18,8 +18,8 @@ define([], function () {
                 var html, li = "";
                 for (var i = begin; i < end && i < data.length; i++) {
                     li += '<li><div class="img-box">' +
-                        '<a class="img-bg" rel="example_group" href="https://github.com/ygjch/ygjch.github.io/photo/' + data[i] + '?raw=true"></a>' +
-                        '<img lazy-src="https://github.com/ygjch/ygjch.github.io/photo/' + data[i] + '?raw=true" />' +
+                        '<a class="img-bg" rel="example_group" href="https://github.com/ygjch/ygjch.github.io/master/photo/' + data[i] + '?raw=true"></a>' +
+                        '<img lazy-src="https://github.com/ygjch/ygjch.github.io/master/photo/' + data[i] + '?raw=true" />' +
                         '</li>';
                 }
 
